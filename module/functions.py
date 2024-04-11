faa9aff93f1044635f0af07eb87da7602115140c
Valen Godoy <valen.c.godoy@gmail.com>
2024-04-11 00:46:37 -0300

    Informacion de goleador

--- module/functions.py
+++ module/functions.py
@@ -1,3 +1,12 @@
+#Se obtiene el diccionario del jugador con mas goles realizados
+def best_player(statistics):
+    player_max_goals = max(statistics, key = lambda player: player["Goles"])
+    return player_max_goals
+
+#Se muestran los datos del goleador
+def info_best_player(statistics):
+    info =  best_player(statistics)
+    return (f'El goleador del partido es {info["Nombre"]} con {info["Goles"]} goles')
 
 
 
